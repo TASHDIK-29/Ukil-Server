bf14734dbc525ce0747fba65278bf23c1d542d96
Tashdik <s2110477153@ru.ac.bd>
2024-12-28 03:16:38 +0600

    advocate api complete

--- index.js
+++ index.js
@@ -39,6 +39,22 @@ async function run() {
 
         const SECRET_KEY = process.env.SECRET_KEY;
 
+        // middleware
+        const verifyToken = (req, res, next) => {
+            console.log('inside verify', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'Unauthorized access!' })
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, SECRET_KEY, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'Unauthorized access!' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
         // Register Advocate
         app.post('/advocate', async (req, res) => {
             const advocate = req.body;
@@ -55,6 +71,8 @@ async function run() {
             const advocateInfo = {
                 name: req.body.name,
                 email: req.body.email,
+                address: req.body.address,
+                city: req.body.city,
                 number: req.body.number,
                 license: req.body.license,
                 yearOfPractice: req.body.yearOfPractice,
@@ -133,7 +151,77 @@ async function run() {
 
         })
 
+        // Advocate info by email
+        app.get('/advocate', verifyToken, async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
 
+            const advocate = await advocatesCollection.findOne(query);
+
+            res.send(advocate);
+        })
+
+        // User info by email
+        app.get('/user', verifyToken, async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+
+            const user = await usersCollection.findOne(query);
+
+            res.send(user);
+        })
+
+        // Get Advocates
+        app.get('/advocates', async (req, res) => {
+            try {
+                // const { search, brand, category, minPrice, maxPrice, currentPage, sort } = req.query;
+                // console.log(search, brand, category, minPrice, maxPrice, currentPage, sort);
+
+                // Build the query object
+                let query = {};
+
+                // if (search) {
+                //     query.$or = [
+                //         { ProductName: { $regex: search, $options: 'i' } },
+                //         { description: { $regex: search, $options: 'i' } }
+                //     ];
+                // }
+
+                // if (brand && brand != "All") {
+                //     query.Brand = brand; // Direct match for a single brand
+                // }
+
+                // if (category && category != "All") {
+                //     query.Category = category; // Direct match for a single category
+                // }
+
+                // if (minPrice && maxPrice) {
+                //     query.Price = { $gte: parseFloat(minPrice), $lte: parseFloat(maxPrice) };
+                // } else if (minPrice) {
+                //     query.Price = { $gte: parseFloat(minPrice) };
+                // } else if (maxPrice) {
+                //     query.Price = { $lte: parseFloat(maxPrice) };
+                // }
+
+
+                
+
+                // Fetch products from MongoDB based on the query
+                const advocates = await advocatesCollection.find(query).toArray();
+                // console.log('advocates=', advocates);
+                const count = advocates.length;
+
+                // const page = parseInt(currentPage);
+                const result = await advocatesCollection.find(query).toArray();
+
+                // console.log('advocates=', result);
+
+                res.send({ count, advocates: result });
+            } catch (error) {
+                res.status(500).json({ error: 'Internal Server Error' });
+                // console.log('does not hit');
+            }
+        });
 
 
 
